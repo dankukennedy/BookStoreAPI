181efa5f6952d0ca82ab6c6e26a56f46ecfd350a
dankukennedy <dankukennedy@gmail.com>
2025-07-28 22:57:42 +0000

    app

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 const bookRoutes = require("./routes/book-routes")
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to our database - use the correct imported function name
 connectToDB();
-
 // Middleware
 app.use(express.json());
-
 //routes
 app.use('/api/books', bookRoutes)
-
 //Server is running on port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
